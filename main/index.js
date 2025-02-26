3b8c53e3bee228070ce73ffa36d6070843fc6960
AngelNcn <97367313+hereticncn@users.noreply.github.com>
2025-02-26 12:47:30 +0300

    feat: 'update slider in index js'

--- main/index.js
+++ main/index.js
@@ -10,31 +10,63 @@ const product = document.querySelectorAll('.pets-content .card')
 let counter = 0;
 
 function left (){
-    if(counter == 0){
-        counter = product.length / 2 - 2
+    if(screenWidth > 1200){
+        if(counter == 0){
+            counter = product.length / 2 - 1
+        }else{
+            counter--
+        }
+    }else if(screenWidth < 780){
+        if(counter == 0){
+            counter = product.length - 2 
+        }else{
+            counter--
+        }
     }else{
-        counter--
+        if(counter == 0){
+            counter = product.length / 1 - 3
+        }else{
+            counter--
+        }
     }
     scroll()
 }
 
 function right(){
-    if(counter == product.length / 2 - 2){
-        counter = 0
-    }else
-
-        counter++
-        scroll()
-    
+    if(screenWidth > 1200){
+        if(counter == product.length / 2 - 1){
+            counter = 0
+        }else{
+            counter++
+        }
+    }else if(screenWidth < 780){
+        console.log(product.length)
+        if(counter == product.length - 2){
+            counter = 0
+        }else{
+            counter++
+        }
+    }else{
+        if(counter == product.length / 1 - 3){
+            counter = 0
+        }else{
+            counter++
+        }
+    }
+    scroll()
 }
 
 function scroll(){
-    product.forEach(function(item) {
-        item.style.transform = `translateX(-${counter * 720}px)`
-    })
-    
+    if(screenWidth > 1200){
+        product.forEach(function(item) {
+            item.style.transform = `translateX(-${counter * 720}px)`
+        })
+    }else{
+        product.forEach(function(item) {
+            item.style.transform = `translateX(-${counter * 310}px)`
+        })
+    }
 }
-
 
 
 
